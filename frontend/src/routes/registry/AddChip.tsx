b2afc1a1a10011fbb5b95ede6c54f13b6f4b206b
Anna Eklund <anna.eklund@yahoo.se>
2025-04-14 14:00:29 +0200

    Add component and basic styling for adding chip

---
+++ frontend/src/routes/registry/AddChip.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import "./AddChip.css";
+
+interface AddChipProps {
+  onAddSuccess: () => void;
+}
+
+function AddChip({ onAddSuccess }: AddChipProps) {
+  const [formData, setFormData] = useState({
+    name: "",
+    use: "",
+    type: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const jsonData = JSON.stringify(formData);
+
+    console.log("jsonData", jsonData);
+
+    fetch("/api/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jsonData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        onAddSuccess();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const isSubmitDisabled = !(formData.name && formData.use && formData.type);
+
+  return (
+    <div className="add-chip-form-ctn">
+      <form onSubmit={handleSubmit} aria-labelledby="form-title">
+        <fieldset>
+          <legend id="form-title">Add new chip</legend>
+          <div className="add-chip-ctn">
+            <div className="form-groups-ctn">
+              <div className="add-chip-labels">
+                <label htmlFor="name">Name:</label>
+                <label htmlFor="use">Use:</label>
+                <label htmlFor="type">Type:</label>
+              </div>
+
+              <div className="add-chip-inputs">
+                <input
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                  value={formData.name}
+                  required
+                  aria-required="true"
+                  aria-invalid={!formData.name}
+                />
+                <input
+                  id="use"
+                  name="use"
+                  onChange={handleChange}
+                  value={formData.use}
+                  required
+                  aria-required="true"
+                  aria-invalid={!formData.use}
+                />
+                <input
+                  id="type"
+                  name="type"
+                  onChange={handleChange}
+                  value={formData.type}
+                  required
+                  aria-required="true"
+                  aria-invalid={!formData.type}
+                />
+              </div>
+            </div>
+
+            <div className="add-chip-form-button">
+              <button
+                type="submit"
+                disabled={isSubmitDisabled}
+                aria-disabled={isSubmitDisabled}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default AddChip;
---
+++ frontend/src/routes/registry/Registry.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState, useCallback } from "react";
+import "./Registry.css";
+import axios from "axios";
+import robot from "../../assets/RobotAtMechanexus.png";
+import AddChip from "./AddChip";
+// import ChipTable from "./ChipTable";
+
+interface ApiResponse {
+  chip_id: number;
+  chip_name: string;
+  chip_use: string;
+  item_type_name: string;
+}
+
+function Home() {
+  const [data, setData] = useState<ApiResponse[] | null>(null),
+    [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const setDataCallback = useCallback((data: ApiResponse[] | null) => {
+    setData(data);
+    console.log("Registry data", data);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("/api")
+      .then((response) => {
+        setDataCallback(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  }, [setDataCallback]);
+
+  function fetchDataAndReload() {
+    axios
+      .get("/api")
+      .then((response) => {
+        setDataCallback(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <div>
+        <div className="registry-wrapper">
+          <div className="registry">
+            <h1>Registry</h1>
+            <AddChip onAddSuccess={fetchDataAndReload} />
+            <h2 className="registry-h2">Registered chips</h2>
+            {/* <ChipTable data={data} isLoading={isLoading}/> */}
+            <img src={robot} alt="Robot" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
