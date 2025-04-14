dcf9681083c2b81fd374b2531c375f4f366b96d9
Anna Eklund <anna.eklund@yahoo.se>
2025-04-14 14:01:57 +0200

    Add route based folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Home from "./components/Home";
-import Registry from "./components/Registry";
+import Home from "./routes/home/Home";
+import Registry from "./routes/registry/Registry";
 import NavBar from "./components/NavBar";
 
 import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
